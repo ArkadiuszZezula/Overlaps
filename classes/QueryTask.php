8c6aa07477a1645cfd8a617234924318a5607f59
Arkadiusz Zezula <arkadiusz.zezula@gmail.com>
2017-06-24 21:25:02 +0200

    added mysql query

--- classes/QueryTask.php
+++ classes/QueryTask.php
@@ -10,5 +10,10 @@ class QueryTask {
      * Final query
      * @var string
      */
-    public static $yourQuery = "Write your query here.";
+    public static $yourQuery = "SELECT * FROM req_docs 
+                                LEFT JOIN pers_docs ON req_docs.doc_nid = pers_docs.doc_nid AND req_docs.pos_id = pers_docs.pos_id
+                                WHERE pers_docs.pers_nid IS NULL OR pers_docs.pers_nid = 6 
+                                AND pers_docs.expires < '2009-09-01'
+                                AND req_docs.pos_id = 'CPT'";
 }
+
